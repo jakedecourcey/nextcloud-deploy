c469323f193307f5851bb57eb828e1775e92117b
Jacob <jakedecourcey@gmail.com>
2020-09-15 12:59:28 -0400

    Added ssh keys for admin access

--- test/deploy-test-environment.tf
+++ test/deploy-test-environment.tf
@@ -64,3 +64,7 @@ output "instance_ip_addr" {
   value = aws_instance.test-instance.public_ip
 }
 
+resource "aws_key_pair" "prod-keys" {
+  key_name = "nextcloud-keys"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCus5+64KBreEk9PC8EvsqsfXuhH6cPq5sgJOfWm+Sse3Ow4w+xagsBZSoJsjRL5PsMW2Jn9DnEsAHtPO7e+ZB3vSVh1ECWp9E+4UA2mHaNqYDjJ16r9T84aCAbhRNFNx0YX0gIDt3vRJsCuwgHIZYsp1E82h6jSAimA1cC/I4Y7izCS9m4w5Q2IZ0sictCpaT/t/UpDwwIxPws+ixlmOaVQPyDNRbqjRWEz7/yft6Ene6GGugPj/vVJ/eAtVQcMUlVsniCUCXCS3aA+Fwh89DKiwE88tnEiankBHR9+geYD64Dn2G87VCKNisvZFELMryRmIit+J+WnWf5dzHJAttCJfCWoEc5qm1N14ns2qvYsXM9zaeTx/UMk8W49+4ogMasgW5RaO9kWJBusO3fNm6L9kKs5nZV5MCQb2JpBgwNUWgNq17LIXEX/I3yn5tynOc+jbWnyOyF4+pfbZm3DX2B9tyNfa3EFAuX+VLukQ2zysIOI3QV77Sl7z8pINgnZKk= jacob@in-amber-clad"
+}
